9dc738eeddd89bb00c1cd3c9bd20729aa23e725d
LittleMewtwo <kamil.ejjennane@yahoo.com>
2025-04-04 09:37:05 +0200

    Polished Hangman game.

--- Hangman Game/Hangman Game/Program.cs
+++ Hangman Game/Hangman Game/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -10,28 +12,61 @@ namespace Hangman_Game
 {
     internal class Program
     {
-        private static List<string> exposed_letters = new List<string>();
+        private static int wrongGuesses = 0;
+        private static List<char> exposed_letters = new List<char>();
+        private static bool win = false;
+        private static int limit;
         static void Main(string[] args)
         {
+
             string input;
-            string word = "hangman";
+            string word;
+            List<string> words = new List<string>() {"strength", "minecraft", "america", "datacrab", "pixelart", "encyclopedia"};
+
+            while (true) {
+                Console.WriteLine("Pick a word or a random word(r) from our provided hangman word list!");
+                input = Console.ReadLine();
+                if (input.ToLower() == "r")
+                {
+                    Random rnd = new Random();
+                    word = words[rnd.Next(0, words.Count)];
+                    break;
+                }
+                else
+                {
+                    if (input.Trim() != "" && input.All(char.IsLetter))
+                    {
+                        word = input.Trim();
+                        break;
+                    }
+                    else
+                        Console.WriteLine("Invalid Input");
+
+                }
+            }
+
+            limit = Convert.ToInt32(0.75 * word.Length);
 
             while (true) // Main Game Loop
             {
-                Console.Clear();
+                // Check if won
+                if (win)
+                    break;
                 // Display Word Method 
                 DisplayWord(word);
 
 
                 Console.WriteLine("Guess letter or word: ");
                 input = Console.ReadLine();
-                if (input.Trim().Length < 2 && input.Trim() != "")
+                if (input.Trim().Length == 1 && char.IsLetter(input[0]))
                 {
-                    if (!exposed_letters.Contains(input.ToLower()))
+                    if (!exposed_letters.Contains(char.ToLower(input[0])))
                     {
-                        GuessLetter(input, word);
-                        if (CheckWinCon(word))
+                        GuessLetter(char.ToLower(input[0]), word);
+                        // Check if lost
+                        if (wrongGuesses > limit)
                             break;
+                        win = CheckWinCon(word);
                     }
                     else
                     { 
@@ -40,25 +75,34 @@ namespace Hangman_Game
                 }
                 else if (input.Trim().Length > 1)
                 {
-                    if(GuessWord(input, word))
-                    {
-                        break;
-                    }
+                    win = GuessWord(input, word);
                 }
                 else
                     Console.WriteLine("Invalid Input");
             }
-            Console.Clear();
-            DisplayWord(word);
-            Console.WriteLine("CONGRATULATIONS!! ");
-            Console.ReadLine();
+            if (win)
+            {
+                DisplayWord(word);
+                Console.WriteLine("Congratulations, you won! ");
+                Console.ReadLine();
+                
+            }
+            else
+            {
+                DisplayWord(word);
+                Console.WriteLine("You lost!");
+                Console.ReadLine();
+            }
         }
 
         static void DisplayWord(string word)
         {
+            Console.Clear();
+            Console.WriteLine("Incorrect Guesses: " + wrongGuesses);
+            Console.WriteLine("Guess Limit: " + limit);
             for (int i = 0; i < word.Length; i++)
             {
-                if (exposed_letters.Contains(word[i].ToString()))
+                if (exposed_letters.Contains(word[i]))
                 {
                     Console.Write(word[i]);
                 }
@@ -68,11 +112,16 @@ namespace Hangman_Game
             Console.WriteLine();
         }
 
-        static void GuessLetter(string letter, string word)
+        static void GuessLetter(char letter, string word)
         {
             if (word.Contains(letter) && !exposed_letters.Contains(letter))
             {
                 exposed_letters.Add(letter);
+            }
+            else
+            {
+                wrongGuesses += 1;
+                Console.WriteLine("Wrong guess!");
             }
         }
 
@@ -80,6 +129,11 @@ namespace Hangman_Game
         {
             if (guessWord.ToLower() == word)
             {
+                foreach (char c in word)
+                {
+                    if (!exposed_letters.Contains(c))
+                        exposed_letters.Add(c);
+                }
                 return true;
             }
             return false;
@@ -89,7 +143,7 @@ namespace Hangman_Game
         {
             foreach (char c in word)
             {
-                if (!exposed_letters.Contains(c.ToString()))
+                if (!exposed_letters.Contains(c))
                     return false;
             }
             return true;
